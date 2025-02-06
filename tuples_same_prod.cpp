186b12c4434cad1b604301ea8ff3d7d6e4235a79
KrishWadhwa24 <krishwadhwa2005@gmail.com>
2025-02-06 23:52:56 +0530

    6/2/2025

---
+++ tuples_same_prod.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Given an array nums of distinct positive integers, return the number of tuples (a, b, c, d) such that a * b = c * d where a, b, c, and d are elements of nums, and a != b != c != d.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,4,6]
+Output: 8
+Explanation: There are 8 valid tuples:
+(2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+(3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
+Example 2:
+
+Input: nums = [1,2,4,5,10]
+Output: 16
+Explanation: There are 16 valid tuples:
+(1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2)
+(2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
+(2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
+(4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 10^4
+All elements in nums are distinct.
+*/
+
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(int i =0;i<nums.size();i++)
+        {
+            for(int j =i+1;j<nums.size();j++)
+            {
+                mpp[nums[i]*nums[j]]++;
+            }
+        }
+        int ans =0;
+        for(auto it:mpp)
+        {
+            cout<<it.first<<" "<<it.second<<"\n";
+            if(it.second>=2)
+            {
+                ans += (it.second*(it.second-1))/2;
+            }
+        }
+        return 8*ans;
+    }
+};
+
+/*
+Time Complexity: O(N^2)
+Space Complexity: O(N)
+*/
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {2,3,4,6};
+    cout<<s.tupleSameProduct(nums)<<"\n";
+    return 0;
+}
