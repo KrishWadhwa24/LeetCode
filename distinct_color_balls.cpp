298b93ee3f550118a52dd3b01948d84a531746db
KrishWadhwa24 <krishwadhwa2005@gmail.com>
2025-02-09 17:03:48 +0530

    9/2/2025

--- distinct_color_balls.cpp
+++ distinct_color_balls.cpp
@@ -10,8 +10,6 @@ Return an array result of length n, where result[i] denotes the number of distin
 
 Note that when answering a query, lack of a color will not be considered as a color.
 
-
-
 Example 1:
 
 Input: limit = 4, queries = [[1,4],[2,5],[1,3],[3,4]]
@@ -19,8 +17,6 @@ Input: limit = 4, queries = [[1,4],[2,5],[1,3],[3,4]]
 Output: [1,2,2,3]
 
 Explanation:
-
-
 
 After query 0, ball 1 has color 4.
 After query 1, ball 1 has color 4, and ball 2 has color 5.
@@ -34,17 +30,13 @@ Output: [1,2,2,3,4]
 
 Explanation:
 
-
-
 After query 0, ball 0 has color 1.
 After query 1, ball 0 has color 1, and ball 1 has color 2.
 After query 2, ball 0 has color 1, and balls 1 and 2 have color 2.
 After query 3, ball 0 has color 1, balls 1 and 2 have color 2, and ball 3 has color 4.
 After query 4, ball 0 has color 1, balls 1 and 2 have color 2, ball 3 has color 4, and ball 4 has color 5.
 
-
 Constraints:
-
 1 <= limit <= 10^9
 1 <= n == queries.length <= 105
 queries[i].length == 2
