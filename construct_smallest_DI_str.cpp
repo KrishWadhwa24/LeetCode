4a1f9ab0da48d0d80c332e8a1ac0acbfdaa3b0a6
KrishWadhwa24 <krishwadhwa2005@gmail.com>
2025-02-18 19:15:58 +0530

    18/2/2025

--- construct_smallest_DI_str.cpp
+++ construct_smallest_DI_str.cpp
@@ -42,6 +42,37 @@ pattern consists of only the letters 'I' and 'D'.
 
 class Solution
 {
+public:
+    string smallestNumber(string pattern)
+    {
+        stack<int> st;
+        string str;
+        for (int i = 0; i <= pattern.size(); i++)
+        {
+            st.push(i + 1);
+
+            if (i == pattern.size() || pattern[i] == 'I')
+            {
+                while (!st.empty())
+                {
+                    str += to_string(st.top());
+                    st.pop();
+                }
+            }
+        }
+        return str;
+    }
+};
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+/*
+Approach: Backtracking
+class Solution
+{
 private:
     bool backtrack(string &pattern, unordered_set<int> &used, string &str, int index, int n)
     {
@@ -107,7 +138,6 @@ public:
     }
 };
 
-/*
 Time Complexity: O(9 * 8 * 7 * ... * (9 - n + 1)) = O((9 - n + 1)!) = O(n!)
 Space Complexity: O(n)
 */
