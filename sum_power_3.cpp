9691e1e5fe6289185d439c8ec0035f44ed0d69e1
KrishWadhwa24 <krishwadhwa2005@gmail.com>
2025-03-05 18:56:28 +0530

    4/5/2025

--- sum_power_3.cpp
+++ sum_power_3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
 Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
 
-An integer y is a power of three if there exists an integer x such that y == 3x.
+An integer y is a power of three if there exists an integer x such that y == 3^x.
 
 
 
@@ -12,12 +12,12 @@ Example 1:
 
 Input: n = 12
 Output: true
-Explanation: 12 = 31 + 32
+Explanation: 12 = 3^1 + 3^2
 Example 2:
 
 Input: n = 91
 Output: true
-Explanation: 91 = 30 + 32 + 34
+Explanation: 91 = 3^0 + 3^2 + 3^4
 Example 3:
 
 Input: n = 21
